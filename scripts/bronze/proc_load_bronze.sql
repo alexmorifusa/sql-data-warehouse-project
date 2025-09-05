a5cb3a143651a435c9773923a918ba6d3a561c3f
Alex Morifusa <137368881+alexmorifusa@users.noreply.github.com>
2025-09-05 15:31:49 -0700

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -69,9 +69,9 @@ CREATE Table bronze.erp_cust_az12(
 	gen NVARCHAR(50)
 );
 
-IF OBJECT_ID ('bronze.erp_px_car_g1v2' , 'U') IS NOT NULL
-	DROP TABLE bronze.erp_px_car_g1v2;
-CREATE Table bronze.erp_px_car_g1v2(
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE Table bronze.erp_px_cat_g1v2(
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
 	subcat NVARCHAR(50),
