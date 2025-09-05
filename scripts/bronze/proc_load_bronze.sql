bc7f4b75fccbe8419477fd866abc4cd4756d406d
Alex Morifusa <137368881+alexmorifusa@users.noreply.github.com>
2025-09-05 15:28:13 -0700

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -43,7 +43,7 @@ CREATE TABLE bronze.crm_prd_info (
 IF OBJECT_ID ('bronze.crm_sales_details' , 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
-	sls_ord_nm NVARCHAR(50),
+	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
 	sls_order_dt INT,
